32e65e2c4774ef797e759b32ae0fe6b774b21b72
Samuel Nicol <99494967+Github-Samuel@users.noreply.github.com>
2023-03-28 17:56:21 +0200

    Script optimization

--- server/server.lua
+++ server/server.lua
@@ -1,20 +1,23 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-QBCore.Functions.CreateCallback('phade-aipolice:server:GetCops', function(source, cb)
-    local amount = 0
-    local players = QBCore.Functions.GetQBPlayers()
-    for _, Player in pairs(players) do
-        for k, v in pairs(Config.PoliceJobs) do
-            if Player.PlayerData.job.name == Config.PoliceJobs[k].job then
-                if Config.PoliceJobs[k].dutyCheck then
-                    if Player.PlayerData.job.onduty then
+CreateThread(function()
+    while true do
+        local amount = 0
+        local players = QBCore.Functions.GetQBPlayers()
+        for _, Player in pairs(players) do
+            for k, v in pairs(Config.PoliceJobs) do
+                if Player.PlayerData.job.name == Config.PoliceJobs[k].job then
+                    if Config.PoliceJobs[k].dutyCheck then
+                        if Player.PlayerData.job.onduty then
+                            amount = amount + 1
+                        end
+                    else
                         amount = amount + 1
                     end
-                else
-                    amount = amount + 1
                 end
             end
         end
+        TriggerClientEvent('phade-aipolice:refresh',-1,amount)
+        Wait(1 * 60 * 1000) -- 1 minute
     end
-    cb(amount)
 end)
