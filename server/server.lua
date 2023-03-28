95796ad9208b4350bc9ce7dc046cb9f81c72ef8d
Samuel Nicol <99494967+Github-Samuel@users.noreply.github.com>
2023-03-28 17:59:10 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -21,3 +21,22 @@ CreateThread(function()
         Wait(1 * 60 * 1000) -- 1 minute
     end
 end)
+
+QBCore.Functions.CreateCallback('phade-aipolice:server:GetCops', function(source, cb)
+     local amount = 0
+     local players = QBCore.Functions.GetQBPlayers()
+     for _, Player in pairs(players) do
+         for k, v in pairs(Config.PoliceJobs) do
+             if Player.PlayerData.job.name == Config.PoliceJobs[k].job then
+                 if Config.PoliceJobs[k].dutyCheck then
+                     if Player.PlayerData.job.onduty then
+                         amount = amount + 1
+                     end
+                 else
+                     amount = amount + 1
+                 end
+             end
+         end
+     end
+     cb(amount)
+ end)
